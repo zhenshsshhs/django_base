ff8575e4a0833b231c2be4af5a180c192bb77710
zheng <352326430@qq.com>
2022-05-18 01:28:09 -0700

    add view

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+VIEW
+ the view is the function in Python.It has two requirement.
+ 1. the first augment is the route of request
+ 2. return HttpResponse
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('ok')
